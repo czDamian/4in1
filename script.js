6ce621101e4ea9bead2896683cbd5fc03e851175
Damian <olebueziedamian@gmail.com>
2024-07-19 13:01:46 +0100

    ui update

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 const startElem = document.getElementById("start");
 const successElem = document.getElementById("success");
 const failElem = document.getElementById("failure");
+const outOfTriesElem = document.getElementById("outOfTries");
+const playPassElem = document.getElementById("playPass");
+const playBalElem = document.getElementById("playBal");
 
 // let currCount = parseFloat(currCountElem.innerHTML);
 let start = startElem.innerHTML;
 let success = successElem.innerHTML;
 let failure = failElem.innerHTML;
+let outOfTries = outOfTriesElem.innerHTML;
+let playBal = parseFloat(playBalElem.innerHTML);
+let playPass = parseFloat(playPassElem.innerHTML);
 
 function increase() {
   currCount++;
@@ -31,20 +37,33 @@ function addLetter(letter) {
     failElem.style.display = "none";
   }
 }
+function updatePassAndBal() {
+  playPassElem.innerHTML = playPass;
+  playBalElem.innerHTML = playBal;
+}
 
 function checkAnswer() {
   const answerSpans = document.querySelectorAll("#answer span");
   const answer = Array.from(answerSpans)
     .map((span) => span.innerText)
     .join("");
-  if (answer === "CAT") {
-    successElem.style.display = "block";
-    failElem.style.display = "none";
-    resetAnswer();
+
+  if (playPass > 0) {
+    if (answer === "CAT") {
+      successElem.style.display = "block";
+      playPass -= 3;
+      playBal += 6;
+      updatePassAndBal();
+      failElem.style.display = "none";
+      resetAnswer();
+    } else {
+      failElem.style.display = "block";
+      successElem.style.display = "none";
+      resetAnswer();
+    }
   } else {
-    failElem.style.display = "block";
+    outOfTriesElem.style.display = "block";
     successElem.style.display = "none";
-    resetAnswer();
   }
 }
 function checkAnswer2() {
@@ -52,14 +71,23 @@ function checkAnswer2() {
   const answer = Array.from(answerSpans)
     .map((span) => span.innerText)
     .join("");
-  if (answer === "HOUSE") {
-    successElem.style.display = "block";
-    failElem.style.display = "none";
-    resetAnswer();
+
+  if (playPass > 0) {
+    if (answer === "HOUSE") {
+      playPass -= 3;
+      playBal += 6;
+      updatePassAndBal();
+      successElem.style.display = "block";
+      failElem.style.display = "none";
+      resetAnswer();
+    } else {
+      failElem.style.display = "block";
+      successElem.style.display = "none";
+      resetAnswer();
+    }
   } else {
-    failElem.style.display = "block";
+    outOfTriesElem.style.display = "block";
     successElem.style.display = "none";
-    resetAnswer();
   }
 }
 
